11d53656db36a779c76ca1e31e6745627717534c
Chauncey Thorn <chaunceyt@gmail.com>
2009-09-07 15:58:16 -0400

    minor change

--- app/config/database.php
+++ app/config/database.php
@@ -6,8 +6,8 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'vibespin',
-		'database' => 'test_db',
+		'password' => 'password',
+		'database' => 'reportfiles',
 	);
 }
 ?>
